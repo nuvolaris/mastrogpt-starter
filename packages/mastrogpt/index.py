295fb1c168fdcec6d3b7abde8b0022fd21e5a3b8
Michele Sciabarra <michele@nuvolaris.io>
2024-01-11 11:09:45 +0000

    fixes

--- packages/mastrogpt/index.py
+++ packages/mastrogpt/index.py
@@ -1,15 +1,21 @@
+import json
+
+
 def main(arg):
-    return {
-        "body": {
-            "services": [
-                {
-                    "name": "Echo",
-                    "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/echo"
-                },
-                {
-                    "name": "Reverse",
-                    "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/reverse"
-                }
-            ]
-        }
+    data = {
+        "services": [
+            {"name": "Echo", "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/echo"},
+            {"name": "Reverse", "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/reverse"},
+            {
+                "name": "HelloOpenAI",
+                "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/hello-openai",
+                "parameters": {"name": "OPENAI_API_KEY", "type": "textfield"},
+            },
+            {
+                "name": "HelloKube",
+                "url": "https://nuvolaris.dev/api/v1/web/dashboard/sample/hello-kube",
+                "parameters": {"name": "KUBECONFIG", "type": "textarea"},
+            },
+        ]
     }
+    return {"body": data}
--- packages/sample/openai.py
+++
@@ -1,6 +0,0 @@
-import openai
-
-def main(args):
-    return {
-        "body": f"OpenAI {openai.__version__}"
-    }
