e01cccb00fa89a03c11a05cd617e98c61c7a563c
Antonio Piga <antonio.piga93@gmail.com>
2024-01-22 17:47:56 +0100

    feat: envs

--- packages/calendar/demo/__main__.py
+++ packages/calendar/demo/__main__.py
@@ -1,18 +1,18 @@
 from google_auth_oauthlib.flow import Flow
 import uuid
 
-def main():
+def main(args):
     
     web_client_info = {
        "web": {
-            "client_id": "xxx",
-            "project_id": "xxx",
-            "auth_uri": "xxx",
-            "token_uri": "xxx",
-            "auth_provider_x509_cert_url": "xxx",
-            "client_secret": "xxx",
-            "redirect_uris": ["xxx"],
-            "javascript_origins": ["xxx"]
+            "client_id": args.get('CLIENT_ID'),
+            "project_id": args.get('PROJECT_ID'),
+            "auth_uri": args.get('AUTH_URI'),
+            "token_uri": args.get('TOKEN_URI'),
+            "auth_provider_x509_cert_url": args.get('AUTH_PROVIDER_X509_CERT_URL'),
+            "client_secret": args.get('CLIENT_SECRET'),
+            "redirect_uris": args.get('REDIRECT_URIS_0'),
+            "javascript_origins": args.get('JAVASCRIPT_ORIGINS_0')
         }  
     }
 
@@ -21,7 +21,7 @@ def main():
 
     flow = Flow.from_client_config(
         web_client_info, scopes,
-        redirect_uri='https://nuvolaris.dev/api/v1/web/antoniopiga/callback/callback')
+        redirect_uri=args.get('URL_TO_REDIRECT_CALLBACK')
 
     authorization_url, _ = flow.authorization_url(prompt='consent')
     print('authorization_url evaluated', authorization_url)
