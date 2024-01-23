83454b3056823c3c61278925f5ed0cdb71e9103b
Antonio Piga <antonio.piga93@gmail.com>
2024-01-23 22:16:27 +0100

    refactor: clean old code and packages, refactor taskfile

--- packages/googlecalendar/chat.py
+++ packages/googlecalendar/chat.py
@@ -1,25 +1,26 @@
 from openai import OpenAI
 
-ROLE = "You are a an assistant that reads google calendar events and describe them in human terms"
+ROLE = "You are an assistant that reads calendar events sent by the user and describes them in human terms"
 MODEL = "gpt-3.5-turbo"
 AI = None
 
-def req(msg):
+def req(user_input):
+    print('user_input is ' + str(user_input))
     return [{"role": "system", "content": ROLE}, 
-            {"role": "user", "content": msg}]
+            {"role": "user", "content": str(user_input)}]
 
-def ask(input):
-    comp = AI.chat.completions.create(model=MODEL, messages=req(input))
+def ask(user_input):
+    comp = AI.chat.completions.create(model=MODEL, messages=req(user_input))
     if len(comp.choices) > 0:
         return comp.choices[0].message.content
     return "ERROR"
 
-
 def main(args):
+    print('input is', args.get('input'))
     global AI
     AI = OpenAI(api_key=args.get("OPEN_AI_KEY_CALENDAR"))
 
-    input = args.get("input", "")
-    output = ask(input)
+    user_input = args.get("input", "")
+    output = ask(user_input)
 
     return {"body": {"output": output}}
